9115afe75641480eb4b7787e95f55d3dffda0abd
Glynn Bird <glynn.bird@gmail.com>
2016-04-29 17:36:08 +0100

    cors please

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
+var request = require('request');
 require('http').createServer(function(req, res) {
   if (req.url.indexOf("/search") == 0) 
-    return require('request')("https://d14f43e9-5102-45bc-b394-c92520c2c0bd-bluemix.cloudant.com/devcenter/_design/search/_search" + req.url).pipe(res);
-  res.end("");
+    request.get("https://d14f43e9-5102-45bc-b394-c92520c2c0bd-bluemix.cloudant.com/devcenter/_design/search/_search" + req.url, function(e,r,body) {
+      res.writeHead(200, { "Access-Control-Allow-Origin": "*" } );
+      res.end(body); 
+    });
 }).listen(require('cfenv').getAppEnv().port);
