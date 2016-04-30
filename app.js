924b2ebb0c9335960f203456e39f00753f0d91e8
Glynn Bird <glynn.bird@gmail.com>
2016-04-30 10:18:12 +0100

    reply to all requests

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 var request = require('request');
 require('http').createServer(function(req, res) {
-  if (req.url.indexOf("/search") == 0) 
+  if (req.url.indexOf("/search") == 0) {
     request.get("https://d14f43e9-5102-45bc-b394-c92520c2c0bd-bluemix.cloudant.com/devcenter/_design/search/_search" + req.url, function(e,r,body) {
       res.writeHead(200, { "Access-Control-Allow-Origin": "*" } );
       res.end(body); 
     });
+  } else {
+    res.end("");
+  }
 }).listen(require('cfenv').getAppEnv().port);
